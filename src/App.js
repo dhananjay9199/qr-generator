d94ca9cea232777d9db7b165be81d56127d33676
dhananjay9199 <dchaudhari758@gmail.com>
2025-04-10 19:59:57 +0530

    Update wedding Invitation Code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from "./logo.svg";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QRCode from "react-qr-code";
 import "./App.css";
 
 function App() {
-  const [url, setUrl] = useState("https://wedding-invitation-psi-one.vercel.app/");
+  const [url, setUrl] = useState(
+    "https://wedding-invitation-psi-one.vercel.app/"
+  );
+
+  useEffect(() => {
+    document.title = "Satish & Mayuri Wedding qr code";
+  }, []);
 
   return (
     <div className="App">
       <h1>SATISH & MAYURI WEDDING INVITATION</h1>
-      {/* <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="Enter URL"
-        style={{ padding: "10px", width: "300px", marginBottom: "20px" }}
-      /> */}
-      <QRCode value={url} />
+      <div className="qr-title">Scan Me</div>
+      <div className="qr-wrapper">
+        <QRCode value={url} />
+      </div>
     </div>
   );
 }
